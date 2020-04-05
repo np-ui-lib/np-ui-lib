588239730db96f5b68662e29683999a3072af8d0
Nilav Patel <nilavpatel1992@gmail.com>
2020-04-05 23:39:30 +0530

    update ui for panel

--- src/app/np-ui-data-grid-demo/inside-tab-grid/inside-tab-grid.component.ts
+++ src/app/np-ui-data-grid-demo/inside-tab-grid/inside-tab-grid.component.ts
@@ -20,7 +20,7 @@ export class InsideTabGridComponent implements OnInit {
       new Column({ dataField: "Id", visible: true, caption: "Id", dataType: DataTypes.Number }),
       new Column({ dataField: "FirstName", visible: true, caption: "First Name", dataType: DataTypes.String }),
       new Column({ dataField: "LastName", visible: true, caption: "Last Name", dataType: DataTypes.String }),
-      new Column({ dataField: "BirthDate", visible: true, caption: "Birth Date", dataType: DataTypes.Date }),
+      new Column({ dataField: "BirthDate", visible: true, caption: "Birth Date", dataType: DataTypes.Date, filterEnable: true}),
       new Column({ dataField: "Age", visible: true, caption: "Age", dataType: DataTypes.Number }),
       new Column({ dataField: "Active", visible: true, caption: "Is Active?", dataType: DataTypes.Boolean })];
 
