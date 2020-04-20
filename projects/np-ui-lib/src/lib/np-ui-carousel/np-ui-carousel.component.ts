3c2d19a5bd7ff2c5776f47e671f99bdfdc6e274e
Nilav Patel <nilavpatel1992@gmail.com>
2020-04-20 22:13:54 +0530

    add new scrollnum property to carousel

--- projects/np-ui-lib/src/lib/np-ui-carousel/np-ui-carousel.component.ts
+++ projects/np-ui-lib/src/lib/np-ui-carousel/np-ui-carousel.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnDestroy, TemplateRef, OnChanges, HostListener, AfterContentInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnDestroy, TemplateRef, HostListener, AfterContentInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'np-ui-carousel',
   templateUrl: './np-ui-carousel.component.html',
   styleUrls: ['./np-ui-carousel.component.css']
 })
-export class NpUiCarouselComponent implements AfterContentInit, OnDestroy, OnChanges {
+export class NpUiCarouselComponent implements AfterContentInit, OnDestroy {
 
   @Input() items: any[] = [];
   @Input() autoPlay: boolean;
@@ -13,12 +13,12 @@ export class NpUiCarouselComponent implements AfterContentInit, OnDestroy, OnCha
   @Input() styleClass: string;
   @Input() itemTemplate: TemplateRef<any>;
   @Input() visibleNum: number = 1;
+  @Input() scrollNum: number = 1;
   @Input() currentPage: number = 0;
   @Input() pauseOnHover: boolean;
   @Input() showNavigationArrows: boolean = true;
   @Input() showNavigationIndicators: boolean = true;
 
-  _totalPages: number;
   _interval: any;
   _start: number;
   _end: number;
@@ -36,26 +36,27 @@ export class NpUiCarouselComponent implements AfterContentInit, OnDestroy, OnCha
     }
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes.items && changes.items.currentValue) {
-      this._totalPages = Math.round(this.items.length / this.visibleNum);
+  _totalPages() {
+    if (this.items == undefined || this.items == null) {
+      return 0;
     }
+    return Math.ceil((this.items.length - this.visibleNum + this.scrollNum) / this.scrollNum);
   }
 
   _getSlidesFromPage() {
-    this._start = (this.currentPage * this.visibleNum);
+    this._start = this.currentPage * this.scrollNum;
     this._end = this._start + this.visibleNum - 1;
   }
 
   _plusSlides() {
     var idx = this.currentPage + 1;
-    this.currentPage = idx >= this._totalPages ? 0 : idx;
+    this.currentPage = idx >= this._totalPages() ? 0 : idx;
     this._getSlidesFromPage();
   }
 
   _minusSlides() {
     var idx = this.currentPage - 1;
-    this.currentPage = idx < 0 ? (this._totalPages - 1) : idx;
+    this.currentPage = idx < 0 ? (this._totalPages() - 1) : idx;
     this._getSlidesFromPage();
   }
 
