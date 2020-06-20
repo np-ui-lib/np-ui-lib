8f66e8062a8f31fd925d3ada454681c5813ac9c0
Nilav Patel <nilavpatel1992@gmail.com>
2020-06-20 23:07:09 +0530

    resolved indeterminate issue

--- projects/np-ui-lib/src/lib/np-tree-view/np-tree-view.module.ts
+++ projects/np-ui-lib/src/lib/np-tree-view/np-tree-view.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NpTreeViewComponent } from './np-tree-view.component';
+import { NpUtilityModule } from '../np-utility/np-utility.module';
 
 @NgModule({
   declarations: [NpTreeViewComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NpUtilityModule
   ],
   exports: [NpTreeViewComponent]
 })
