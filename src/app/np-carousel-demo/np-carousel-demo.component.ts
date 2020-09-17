9df5340d26107b4557838bf3db76dfb1707ea7d1
Nilav Patel <nilavpatel1992@gmail.com>
2020-09-17 10:25:46 +0530

    border radius fixes

--- src/app/np-carousel-demo/np-carousel-demo.component.ts
+++ src/app/np-carousel-demo/np-carousel-demo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { NpCarouselComponent } from 'np-ui-lib';
 
 @Component({
