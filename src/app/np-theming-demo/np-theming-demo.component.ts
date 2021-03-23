74b3ee0b0edf1a539791c324867d6171d1a6fffd
Nilav Patel <nilavpatel1992@gmail.com>
2021-03-23 12:11:45 +0530

    remove $outline-size parameter

--- src/app/np-theming-demo/np-theming-demo.component.ts
+++ src/app/np-theming-demo/np-theming-demo.component.ts
@@ -7,13 +7,12 @@ import { Component, OnInit } from '@angular/core';
 export class NpThemingDemoComponent implements OnInit {
 
   customSCSS = `@import "np-ui-lib/styles/theme.scss";
-$primary-color: #0d47a1;// set primary color</span>
+$primary-color: #0b7ad1;// set primary color</span>
 $background-color: #f5f5f5;// set background color</span>
 $selected-color: #eeeeee;// set background color for selected items</span>
 $border-color: #9e9e9e;// set border color</span>
 $border-radius: 2px;// set border radius</span>
-$outline-color: #0d47a150;// set outline color</span>
-$outline-size: 0.2rem;// set outline size</span>
+$outline-color: #a6d5fa;// set outline color</span>
 @include theme;`;
 
   constructor() { }
